d2564c34490ad1b0f6ced78f90f9ebc6a11d35a7
Oladimeji Vincent <60340075+vincey101@users.noreply.github.com>
2026-02-12 22:27:07 +0100

    Update login API endpoint URL

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -27,7 +27,7 @@ export default function Login() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('https://api.humanaiapp.com/api/login', {
+            const response = await fetch('https://api.mysuperaiapp.com/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
